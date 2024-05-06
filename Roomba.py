06c8977331783bf688399f0363f2505577466972
Tomypv <tpv888@usal.es>
2024-05-06 13:51:39 +0200

    Implementación de funcion random

--- Roomba.py
+++ Roomba.py
@@ -1,6 +1,8 @@
 
 from pycreate2 import Create2
 import keyboard  # Librería para detectar las teclas presionadas
+import random
+import time
 
 # Función para controlar el movimiento del Roomba
 def controlar_roomba(tecla):
@@ -23,6 +25,19 @@ def controlar_roomba(tecla):
     else:
         bot.drive_stop()  # Detener el movimiento si no se presiona ninguna tecla de movimiento
 
+def movimiento_aleatorio():
+    while True:
+        # Generar velocidades aleatorias para cada rueda
+        velocidad_izquierda = random.randint(-500, 500)
+        velocidad_derecha = random.randint(-500, 500)
+        bot.drive_direct(velocidad_izquierda, velocidad_derecha)
+        time.sleep(0.5)  # Pausa breve para permitir el movimiento
+
+        # Detener el movimiento si se presiona otra tecla
+        if keyboard.is_pressed('q'):  # Puedes cambiar 'q' por cualquier tecla para detener
+            bot.drive_stop()
+            break
+
 # Crear una instancia de Create2 y establecer la conexión
 port = "COM5"  # Puerto serial donde está conectado el Roomba
 bot = Create2(port)
@@ -30,14 +45,10 @@ bot.start()
 bot.safe()
 
 # Bucle para detectar las teclas presionadas y controlar el Roomba
+keyboard.add_hotkey('r', movimiento_aleatorio)
 keyboard.hook(controlar_roomba)
 keyboard.wait('esc')  # Esperar hasta que se presione la tecla 'Esc' para detener el programa
 
 # Cerrar la conexión al finalizar
 bot.drive_stop()
-
-
-
-
-
 bot.close()
